fbb109e095e213538733b448a23a0ceb89f28ac9
hatembd <hatembendahmen@gmail.com>
2022-08-07 14:00:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const parkings = require('./parkings.json')
+
+// Middleware
+app.use(express.json())
+
+app.get('/parkings', (req,res) => {
+    res.status(200).json(parkings)
+})
+
+app.get('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const parking = parkings.find(parking => parking.id === id)
+    res.status(200).json(parking)
+})
+
+app.listen(8080, () => {
+    console.log("Serveur à l'écoute")
+})
